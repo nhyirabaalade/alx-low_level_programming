378f7672b07fcea5f4c142ee34f1a6dc70be2f3f
nhyirabaalade <nhyirabaajay2@gmail.com>
2021-06-21 13:18:29 -0700

    hi

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,12 @@
 
 int main()
 {
-    for(int x = 'A'; x <= 'Z'; x++)
-    {
-        int lower_x = tolower(x);
-        putchar(lower_x);
-    }
-    return 0;
+	int x;
+	x = 'A';
+	for(x = 'A'; x <= 'Z'; x++)
+	{
+		int lower_x = tolower(x);
+		putchar(lower_x);
+	}
+	return 0;
 }
