773f89a00cd2334c06bf8e752d55e3b3484afa69
nhyirabaalade <nhyirabaajay2@gmail.com>
2021-06-21 07:54:35 -0700

    arrange

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long int l;
 	long long int t;
 
-	printf("The size of an int: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a float: %lu.\n", (unsigned long)sizeof(f));
-	printf("The size of a character: %lu.\n", (unsigned long)sizeof(c));
-	printf("The size of an long int: %lu.\n", (unsigned long)sizeof(l));
-	printf("The size of an long long int: %lu.\n", (unsigned long)sizeof(t));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(t));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
